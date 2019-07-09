3ea0a0fbc7ec10f868bc1a737114b83cb4bc7cfa
Christensen-Dustin <39143972+Christensen-Dustin@users.noreply.github.com>
2019-07-09 00:09:01 -0700

    Teach 12 update

--- server.js
+++ server.js
@@ -37,112 +37,16 @@ app.set('port', (process.env.PORT || localPORT));
 app.use(express.static(path.join(__dirname, "public")));
 
 // To view all middleware function for requests
-// app.use(logRequest);
 app.use(verify.logVerifyRequest);
 
 // Paths
-// app.post('/login',  userLogin);
 app.post('/login', login.userLogin);
-// app.post('/logout', userLogout);
 app.post('/logout', logout.userLogout);
 
 // Middleware function methods
-// app.get('/getServerTime', verifyLogin, getServerTimeStamp);
 app.get('/getServerTime', verify.verifyLogin, verify.getServerTimeStamp);
 
 // Start the SERVER listening
 app.listen(app.get('port'), function() {
     console.log('Server is listening on PORT: ' + app.get('port'));
 });
-
-
-/***************************************************************************
-*   Modules
-***************************************************************************/
-
-function userLogin(request, response) {
-    console.log("userLogin started");
-    
-    // access granted, default FALSE
-    var results = { success: false };
-    
-    // check user input NAME and PASSWORD
-    if(request.body.username == "admin" && request.body.password == "password") {
-        
-        // Transferr to SESSION variable
-        request.session.user = request.body.username;
-        
-        // Change access to granted
-        results = { success: true };
-    };
-    console.log("userLogin ended");
-    console.log(results);
-    
-    response.json(results);
-};
-
-function userLogout(request, response) {
-    console.log("userLogout started");
-    
-    // Is the user logged out, default FALSE
-    var results = { success: false };
-    
-    // Verify that the user is logged out
-    if(request.session.user) {
-        
-        // DESTROY USER Session Variables
-        request.session.destroy();
-        
-        // Change verification to user being logged out to TRUE
-        results = { success: true };
-    };
-    console.log("userLogout ended");
-    console.log(results);
-    
-    response.json(results);
-};
-
-function getServerTimeStamp(request, response) {
-    console.log("getServerTimeStamp started");
-    
-    // Get the Serve time
-    var timeStamp = new Date();
-    
-    // Add time to results
-    var results = { success: true, timeStamp: timeStamp };
-    
-    // return results
-    response.json(results);
-};
-
-function verifyLogin(request, response, next) {
-    console.log("verifyLogin started");
-    
-    // check to see if user is logged in
-    if(request.session.user) {
-        console.log("verifyLogin pass to NEXT()");
-        
-        // Pass results to the NEXT function
-        next();
-        
-    }else{
-        
-        // change verification and generate unauthorized status message
-        var results = { success: false, message: "Unautorized User - Access Denied" };
-        
-        // Send back with 401 status
-        response.status(401).json(results);
-    }
-};
-
-function logRequest(request, response, next) {
-    console.log("logRequest started");
-    
-    // confirm that the URL was passed correctly
-    console.log("Received a request for: " + request.url);
-    
-    // continue on to the NEXT function
-    console.log("logRequest pass to NEXT()");
-    
-    next();
-};
